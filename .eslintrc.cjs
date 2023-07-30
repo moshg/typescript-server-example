443f7ebe2b4e577c256c38bf90f9b0725c5b2f58
mosh <mosh.ohayo@gmail.com>
2023-07-30 15:15:07 +0900

    Add ESLint and Prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    // browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'es2020',
+    sourceType: 'module',
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
